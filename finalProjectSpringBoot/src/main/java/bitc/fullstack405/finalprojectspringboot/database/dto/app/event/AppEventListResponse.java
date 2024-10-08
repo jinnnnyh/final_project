4d0dbece05536722e5a3a2e52031fd313906d809
ejshinn <ejshin.dev@gmail.com>
2024-10-08 16:24:19 +0900

    앱 - 상세 페이지 게시일 추가

--- finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/database/dto/app/event/AppEventListResponse.java
+++ finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/database/dto/app/event/AppEventListResponse.java
@@ -9,14 +9,6 @@ import java.time.format.DateTimeFormatter;
 public class AppEventListResponse {
 
     // 앱 - 회원이 볼 행사 목록
-
-    // 서비스에서 쓸 데이터 : 행사 id, event_accept(2인지 확인), is_registration_open(Y인지 확인)
-    // visible_date <= 현재 날짜 <= invisible_date
-
-    // event 테이블 : event_id, max_people, event_title, event_content, event_poster, upload_date, user_id
-    // event schedule 테이블(event_id로 가져오기) : schedule_id, event_date, start_time, end_time
-
-    // 행사 시작 시간, 종료시간, 작성자(이름)
     private final Long eventId; // 행사 id
     private final String eventTitle; // 제목
     private final String uploadDate; // 등록일
