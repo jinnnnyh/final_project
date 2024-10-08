22a40316e713b51391fe219b575d9b61b9c13da5
PunkidSH <940809psh@naver.com>
2024-10-08 18:46:06 +0900

    병합 후 개인설정 변경

--- finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/database/entity/UserEntity.java
+++ finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/database/entity/UserEntity.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
