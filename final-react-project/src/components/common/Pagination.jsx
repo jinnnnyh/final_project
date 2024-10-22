6e0d046cb42affe70952912e8bc68379a5c0a2a2
PunkidSH <940809psh@naver.com>
2024-10-23 08:54:20 +0900

    이벤트목록 페이징처리 부분 수정(테스트 필요) EventList / Pagination 컴포넌트 수정됨

--- final-react-project/src/components/common/Pagination.jsx
+++ final-react-project/src/components/common/Pagination.jsx
@@ -1,20 +1,18 @@
 import ReactPaginate from 'react-js-pagination';
 
-const Pagination = ({ activePage, itemsCountPerPage, totalItemsCount, onPageChange }) => {
+const Pagination = ({ currentPage, itemsCount, itemsPerPage, onPageChange }) => {
   return (
     <ReactPaginate
-      activePage={activePage}
-      itemsCountPerPage={itemsCountPerPage}
-      totalItemsCount={totalItemsCount}
+      activePage={currentPage}
+      itemsCountPerPage={itemsPerPage}
+      totalItemsCount={itemsCount}
       pageRangeDisplayed={5}
       onChange={onPageChange}
-      innerClass="pagination" // 원하는 클래스명으로 스타일링 가능
-      itemClass="page-item" // 각 페이지 아이템에 대한 클래스명
-      linkClass="page-link" // 링크에 대한 클래스명
+      innerClass="pagination"
+      itemClass="page-item"
+      linkClass="page-link"
     />
   );
 };
 
-
 export default Pagination;
-
