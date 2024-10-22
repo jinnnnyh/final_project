ecc2ebfa2ea6e7c96011e374343c614cf24a818f
hegenius <hegenius1990@gmail.com>
2024-10-22 10:47:25 +0900

    리액트수정 - nav, 버튼(승인거부,승인대기,모집종료), 글쓰기:인원수=>정원수

--- final-react-project/src/components/common/EndEventButton.jsx
+++ final-react-project/src/components/common/EndEventButton.jsx
@@ -5,50 +5,77 @@ import {useParams} from "react-router-dom";
 
 function EndEventButton() {
   const [eventData, setEventData] = useState([]);
-  const { eventId } = useParams();
+  const {eventId} = useParams();
+  const [eventAccept, setEventAccept] = useState('');
+  const [isRegistrationOpen, setIsRegistrationOpen] = useState();
+  const [error, setError] = useState(null);
 
-  const [isButtonDisabled, setIsButtonDisabled] = useState(() => {
-    // 로컬 스토리지에서 해당 ID의 값을 읽기
-    const savedValue = localStorage.getItem(`buttonDisabled-${eventId}`);
-    return savedValue === 'true';
-  });
 
   useEffect(() => {
-    // 상태가 변경될 때 로컬 스토리지에 저장
-    localStorage.setItem(`buttonDisabled-${eventId}`, isButtonDisabled);
-  }, [isButtonDisabled, eventId]);
+    axios
+      .get(`http://localhost:8080/event/${eventId}`)
+      .then((response) => {
+        if (response.data) {
+          setEventData(response.data);
+          setEventAccept(response.data);
+          setIsRegistrationOpen(response.data);
+        } else {
+          setError("데이터를 찾을 수 없습니다.");
+        }
+      })
+      .catch((err) => {
+        setError("서버 오류가 발생했습니다.");
+      });
+  }, [eventId]);
 
+
+  // 모집종료 버튼 (Y : 모집중, N : 모집완료)
+  // 승인대기일때 (1) 모집완료 Y => 승인대기중 alert
+  // 승인완료일때 (2) 모집완료 Y => 모집종료여부 확인 confirm => 모집완료 N
+  // 승인거부일때 (3) => 자동 모집완료 N
   const handleEndEvent = async () => {
-    if (isButtonDisabled) {
-      alert("이미 처리된 내용입니다.");
-      return;
-    }
-
-    // 모집종료 버튼
-    const confirmed = window.confirm('행사 모집 종료하시겠습니까?');
-
-    if (confirmed) {
-      try {
-        const dataToSave = { id: eventId, key: "isRegistrationOpen" };
-        await axios.put(`http://localhost:8080/event/denyEvent/${eventId}`, dataToSave);
-        setEventData(eventData.filter(eventData => eventData.eventId !== eventId));
-        setIsButtonDisabled(true);
-        localStorage.setItem('isButtonDisabled', 'true'); // 상태를 로컬 스토리지에 저장
-        alert("행사 모집종료되었습니다.");
-      } catch (error) {
-        console.error("모집종료 중 오류 발생:", error);
-        alert("모집종료 중 오류가 발생했습니다.");
+    if (eventData.eventAccept === 1) {
+      alert("승인대기 중인 행사입니다.");
+    } else if (eventData.eventAccept === 2) {
+      if (eventData.isRegistrationOpen === 'Y') {
+        const confirmed = window.confirm("행사 모집 종료하시겠습니까?");
+        if (confirmed) {
+          alert("행사 모집종료되었습니다.");
+          window.location.href = `/event/${eventId}`
+          const response = await axios.put(`http://localhost:8080/event/endEvent/${eventId}`)
+          setEventData(eventData.filter(eventData => eventData.eventId !== eventId));
+          setEventAccept(response.data);
+          setIsRegistrationOpen(response.data);
+        } else {
+          // console.error("모집종료 중 오류 발생:", error);
+        }
+      } else if (eventData.isRegistrationOpen === 'N') {
+        alert("이미 모집완료된 행사입니다.");
+      } else {
+        setError("서버 오류가 발생했습니다.");
       }
+    }else if (eventData.eventAccept === 3) {
+      alert("승인거부된 행사입니다.");
+    } else {
+      setError("서버 오류가 발생했습니다.");
     }
-  };
-  
+  }
+
 
   return (
+  <button type={'button'} style={{border: 'none', background: 'none'}} onClick={() => handleEndEvent()}>
+    {eventData.isRegistrationOpen === 'Y' && eventData.eventAccept === 1 &&
+      <span className={'btn'} style={{color:'#aaa', border:'1px solid #ccc', backgroundColor:'#f4f4f4'}}>모집종료</span> ||
 
-    <button type={'button'} className={'btn btn-danger'}  onClick={handleEndEvent} disabled={isButtonDisabled}>
-      {isButtonDisabled ? "모집종료" : "모집종료"}
-    </button>
-  )
+      eventData.isRegistrationOpen === 'Y' && eventData.eventAccept === 2 && <span className={'btn btn-outline-danger'}>모집종료</span> ||
+      eventData.isRegistrationOpen === 'N' && eventData.eventAccept === 2 &&
+      <span className={'btn'} style={{color:'#aaa', border:'1px solid #ccc', backgroundColor:'#f4f4f4'}}>모집종료</span> ||
+
+      eventData.isRegistrationOpen === 'Y' && eventData.eventAccept === 3 &&
+      <span className={'btn'} style={{color:'#aaa', border:'1px solid #ccc', backgroundColor:'#f4f4f4'}}>모집종료</span>
+    }
+  </button>
+)
 }
 
 export default EndEventButton;
