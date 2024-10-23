2184c22c521a3efa437a86c685ed00fbebef259a
ejshinn <ejshin.dev@gmail.com>
2024-10-23 12:01:34 +0900

    웹 - 승인 버튼 한 번만 눌러도 되게 수정 완료, 행사 목록에서 "행사 중" 잘 나오게 수정 완료

--- final-react-project/src/components/common/AcceptEventButton.jsx
+++ final-react-project/src/components/common/AcceptEventButton.jsx
@@ -12,18 +12,18 @@ function AcceptEventButton() {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8080/event/${eventId}`)
-      .then((response) => {
-        if (response.data) {
-          setEventData(response.data);
-          setEventAccept(response.data);
-        } else {
-          setError("데이터를 찾을 수 없습니다.");
-        }
-      })
-      .catch((err) => {
-        setError("서버 오류가 발생했습니다.");
-      });
+        .get(`http://localhost:8080/event/${eventId}`)
+        .then((response) => {
+          if (response.data) {
+            setEventData(response.data);
+            setEventAccept(response.data);
+          } else {
+            setError("데이터를 찾을 수 없습니다.");
+          }
+        })
+        .catch((err) => {
+          setError("서버 오류가 발생했습니다.");
+        });
   }, [eventId]);
 
 
@@ -32,28 +32,32 @@ function AcceptEventButton() {
   const handleAcceptEvent = async() => {
     if(eventData.eventAccept  === 1 || eventData.eventAccept  === 3) {
       const confirmed = window.confirm('행사 승인하시겠습니까?');
-      window.location.href = `/event/${eventId}`
       if (confirmed) {
-        alert("승인되었습니다.");
         const userId = sessionStorage.getItem("userId");
         const response = await axios.put(`http://localhost:8080/event/acceptEvent/${eventId}`, { newValue: '승인완료' }, {
           params: { userId: userId }
         })
+            .then(() => {
+              alert("승인되었습니다.");
+              window.location.href = `/event/${eventId}`
+            })
         setEventData(eventData.filter(eventData => eventData.eventId !== eventId));
         setEventAccept(response.data);
-
       } else {
         // console.error("승인 중 오류 발생:", error);
       }
     } else if (eventData.eventAccept  === 2) {
       const confirmCancel = window.confirm('승인 취소하시겠습니까?');
-      window.location.href = `/event/${eventId}`
+
       if (confirmCancel) {
-        alert("승인취소되었습니다.");
         const userId = sessionStorage.getItem("userId");
         const response = await axios.put(`http://localhost:8080/event/acceptCancel/${eventId}`, { newValue: '승인대기' }, {
           params: { userId: userId }
         })
+            .then(() => {
+              alert("승인취소되었습니다.");
+              window.location.href = `/event/${eventId}`
+            })
         setEventData(eventData.filter(eventData => eventData.eventId !== eventId));
         setEventAccept(response.data);
       } else {
@@ -67,12 +71,12 @@ function AcceptEventButton() {
 
 
   return (
-    <button type={'button'} style={{border:'none', background:'none'}}  onClick={() => handleAcceptEvent()}>
-      {eventData.eventAccept === 1 && <span className={'btn btn-outline-point'} >승인대기</span> ||
-        eventData.eventAccept === 2 && <span className={'btn btn-point'} >승인완료</span> ||
-        eventData.eventAccept === 3 && <span className={'btn btn-outline-point'} >승인대기</span>
-      }
-    </button>
+      <button type={'button'} style={{border:'none', background:'none'}}  onClick={() => handleAcceptEvent()}>
+        {eventData.eventAccept === 1 && <span className={'btn btn-outline-point'} >승인하기</span> ||
+            eventData.eventAccept === 2 && <span className={'btn btn-point'} >승인취소</span> ||
+            eventData.eventAccept === 3 && <span className={'btn btn-outline-point'} >승인대기</span>
+        }
+      </button>
   )
 }
 export default AcceptEventButton;
