6244de4cffc3dda08f7e4dae90ec8afe66dec725
ejshinn <ejshin.dev@gmail.com>
2024-10-23 09:29:38 +0900

    웹 - 행사 상세 페이지에서 최대 인원으로 인한 모집 종료 백엔드 추가

--- finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/service/EventService.java
+++ finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/service/EventService.java
@@ -225,6 +225,12 @@ public class EventService {
             eventScheduleViewDTOList.add(eventScheduleViewDTO);
         }
 
+        char isRegistrationOpen = 'Y';
+
+        if(event.getIsRegistrationOpen() == 'N' || eventRepository.listCheckMaxPeople(event.getEventId())) {
+            isRegistrationOpen = 'N';
+        }
+
         EventViewDTO eventViewDTO = EventViewDTO.builder()
                 .eventId(event.getEventId())
                 .eventTitle(event.getEventTitle())
@@ -232,7 +238,7 @@ public class EventService {
                 .eventPoster(event.getEventPoster())
                 .eventAccept(event.getEventAccept())
                 .maxPeople(event.getMaxPeople())
-                .isRegistrationOpen(event.getIsRegistrationOpen())
+                .isRegistrationOpen(isRegistrationOpen)
                 .acceptedDate(event.getAcceptedDate())
                 .visibleDate(event.getVisibleDate())
                 .invisibleDate(event.getInvisibleDate())
